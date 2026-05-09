df2e9f270db1c30d5d002277d68e607386cd3f77
ssenkaayi <ssenkaaayi@gmail.com>
2026-05-09 08:32:22 +0100

    adding text content to home page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,12 +3,14 @@ import Nav from "./Nav";
 import { Outlet } from "react-router-dom";
 
 function Layout(){
+
     return(
-        <>
+
+        <div className="page-container">
             <Nav/>
             <Outlet/>
             <Footer/>
-        </>
+        </div>
     )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,56 @@
 
 function Home(){
     return(
-        <div>
-        <h1>
-            home page
-        </h1>
+        <div className="page-container">
+            <div className="hero">
+
+                <div hero-content-container>
+                    <h3>
+                        Hello, my name is Charles Ssenkaayi
+                    </h3>
+                    <h1>
+                        And i am a web developer
+                    </h1>
+                    <p>Experience in HTML, CSS and Javascript</p>
+                    <ul>
+                        <li>Resume</li>
+                        <li>Git</li>
+                        <li>LinkedIn</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <body>
+
+                <div className="tech-stack">
+
+                    <h3>Tech stack</h3>
+
+                    <ul>
+                        <li>MongoDb</li>
+                        <li>Express</li>
+                        <li>React</li>
+                        <li>Node</li>
+                    </ul>
+                </div>
+
+                <div className="recent-projects">
+
+                    <h3>Recent projects</h3>
+
+                    <div className="project-card-container">
+                        <div className="project-card">
+                            <img src="" alt="" />
+                            <div>
+                                <h4>Project name</h4>
+                                <p>Description</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </body>
         </div>
 
     )
