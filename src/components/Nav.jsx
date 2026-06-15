5e8ccdccd4f70830b6e56525a8e573aa5d5c2f91
ssenkaayi <ssenkaaayi@gmail.com>
2026-06-15 14:46:45 +0100

    completing mobile svg menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { IoMdMenu } from "react-icons/io";
 
 function Nav(){
     return(
@@ -9,6 +10,27 @@ function Nav(){
                 </div>
 
                 <ul className="nav-menu">
+                    <li><Link className="nav-links" to='/' >Home</Link></li>
+                    <li><Link className="nav-links" to='/skill' >Skill</Link></li>
+                    <li><Link className="nav-links" to='/experience' >Experience</Link></li>
+                    <li><Link className="nav-links" to='/about' >About</Link></li>
+                </ul>
+            </div>
+
+            <div className="mobile-nav">
+                <div className="mobile-nav-menu">
+                    <div className="nav-logo">
+                        Chal2
+                    </div>
+
+                    <div className="mobile-menu">
+                        < IoMdMenu className="mobile-svg-menu"/>
+                        
+                    </div>
+                </div>
+
+
+                <ul className="mobile-nav-links">
                     <li><Link className="nav-links" to='/' >Home</Link></li>
                     <li><Link className="nav-links" to='/skill' >Skill</Link></li>
                     <li><Link className="nav-links" to='/experience' >Experience</Link></li>
