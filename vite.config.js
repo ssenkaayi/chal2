bc83f54c454d0832ecdf6dd0010373b2e1c0af55
ssenkaayi <ssenkaaayi@gmail.com>
2026-06-28 17:39:14 +0100

    adding app entry point

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chal2',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
