def69b8a877669c93f006caa1f13352400cee739
ssenkaayi <ssenkaaayi@gmail.com>
2026-07-01 15:38:04 +0100

    correcting project image paths

--- src/data/data.js
+++ src/data/data.js
@@ -1,27 +1,29 @@
 
 
-// import project1 from "./src/images/project-images/tour.png"
-// import project2 from "./src/images/project-images/quil.png"
-// import project3 from "./src/images/project-images/portfolio.png"
+let project1 = "/chal2/src/images/project-images/tour.png";
+let project2 = "/chal2/src/images/project-images/quil.png";
+let project3 = "/chal2/src/images/project-images/portfolio.png";
+
+
 
 const recentProjects = [
 
     {
         "name":"Tour app",
         "des":"Introduction to the fundamentals of React",
-        "image_path":"chal2/src/images/project-images/tour.png",
+        "image_path":project1, 
         "url":"https://ssenkaayi.github.io/sekai-safaris/"
     },
     {
         "name":"Quil app",
         "des":"Understanding HTML and CSS fundamentals",
-        "image_path":"chal2/src/images/project-images/quil.png",
+        "image_path":project2,
         "url":"https://ssenkaayi.github.io/future-fullstack/"
     },
     {
         "name":"Portifolio app",
         "des":"Mastery of vanilla HTML, CSS and Javascript",
-        "image_path":"chal2/src/images/project-images/portfolio.png",
+        "image_path":project3,
         "url":"https://ssenkaayi.github.io/html-css-portfolio/"
     },
 
